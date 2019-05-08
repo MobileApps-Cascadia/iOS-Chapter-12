fe8a426d8f6b0c0385d134430477b0a18de21e5c
Robert <robertramirez658@gmail.com>
2019-05-07 17:50:28 -0700

    Halfway Done

--- Test TableView/CityDataSource.swift
+++ Test TableView/CityDataSource.swift
@@ -18,15 +18,20 @@ class CityDataSource: NSObject, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // Fetch a cell of the appropriate type.
     
-          let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableCellReuseId", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyTableCellReuseId", for: indexPath) as! MyTableViewCell
         
         //TODO: comment out this configuration for the default cell
               // cell.textLabel!.text = data[indexPath.row]
        
         //TODO: Configure the custom cell’s city and state UILables' text value using the data array item
         
-    
+        
         let item:String = data[indexPath.row] //the first item is "Seattle, WA"
+        
+        let FixedItem = item.components(separatedBy: ", ")
+        
+        cell.CityLabel.text = FixedItem[0]
+        cell.StateLabel.text = FixedItem[1]
         
         
         //HINT: Use the Developer Documentation window to serach for .components(separatedBy
